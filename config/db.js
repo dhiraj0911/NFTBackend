c8707610a293fb5b13493f0419a16b89de239ead
Lauquik <laukik.22010776@viit.ac.in>
2024-02-13 19:49:29 +0530

    uncomment mongo_uri

--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose')
 const connectDB = async () => {
   try {
     await mongoose.connect(
-      // process.env.MONGO_URI,
+      process.env.MONGO_URI ||
       "mongodb://localhost:27017/game-asset",
       {
         useNewUrlParser: true,
