6365dc6a16624495b858cc10a42aa166d09f82c0
dhiraj0911 <djirajbari123@gmail.com>
2024-02-24 21:39:45 +0530

    fix addressController

--- controller/addressController.js
+++ controller/addressController.js
@@ -26,15 +26,13 @@ const getAddressById = async (req, res) => {
 
 const findOneFromVendorAndCreateAddress = async (req, res) => {
     try {
-        const address = await Address.findOne({ vendorId: req.params.vendorId });
-        if (address) {
+        const { vendorId, address } = req.body;
+        const res = await Address.findOne({ vendorId: vendorId });
+        if (res) {
             return res.status(200).json({ message: "Address Already Store" });
         }
-        const newAddress = new Address(req.body);
-        await newAddress.save();
-        res.status(201).json(newAddress);
-        // const address = await Address.create({ vendorId: req.body.vendorId, address: req.body.address });
-        // res.json(address);
+        await Address.create({ vendorId: vendorId, address });
+        res.status(201).json({ response: "OK" });
     } catch (error) {
         console.error(error);
         res.status(500).json({ message: "Server Error" });
